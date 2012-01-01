e402f7788f8daf841a694dde900079d79329843e
Michael Lauer <michael@mrlauer.com>
2012-01-01 18:10:12 -0500

    gofmt

--- mrlutils/mrlutils.go
+++ mrlutils/mrlutils.go
@@ -1,6 +1,6 @@
 package mrlutils
 
-import(
+import (
 	"os"
 	"exec"
 )
@@ -9,16 +9,15 @@ type byteWriter struct {
 	data *[]byte
 }
 
-func (w *byteWriter) Write (b []byte) (n int, err os.Error) {
+func (w *byteWriter) Write(b []byte) (n int, err os.Error) {
 	*w.data = append(*w.data, b...)
 	return len(b), nil
 }
 
-func Execute(cmd string, args... string) (stdout []byte, stderr []byte, err os.Error) {
+func Execute(cmd string, args ...string) (stdout []byte, stderr []byte, err os.Error) {
 	command := exec.Command(cmd, args...)
 	command.Stdout = &byteWriter{&stdout}
 	command.Stderr = &byteWriter{&stderr}
 	err = command.Run()
 	return
 }
-
--- mrlutils/mrlutils_test.go
+++ mrlutils/mrlutils_test.go
@@ -1,6 +1,6 @@
 package mrlutils
 
-import(
+import (
 	"testing"
 	"strings"
 )
