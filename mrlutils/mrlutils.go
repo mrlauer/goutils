1c3276c8c405e455bff11695d75cae03491c086f
Michael Lauer <michael@mrlauer.com>
2012-01-02 08:14:45 -0500

    ReadLines

--- mrlutils/mrlutils.go
+++ mrlutils/mrlutils.go
@@ -3,21 +3,67 @@ package mrlutils
 import (
 	"os"
 	"exec"
+	"bytes"
 )
 
-type byteWriter struct {
-	data *[]byte
+// A writer that appends to an internal byte array
+// (Isn't there a standard package for that?)
+type ByteWriter struct {
+	data []byte
 }
 
-func (w *byteWriter) Write(b []byte) (n int, err os.Error) {
-	*w.data = append(*w.data, b...)
+// Write by appending to the internal array
+func (w *ByteWriter) Write(b []byte) (n int, err os.Error) {
+	w.data = append(w.data, b...)
 	return len(b), nil
 }
 
+// Return the ByteWriter's data
+func (w *ByteWriter) Data() []byte {
+	return w.data
+}
+
+// Create a ByteWriter
+func NewByteWriter() *ByteWriter {
+	return &ByteWriter{make([]byte, 0)}
+}
+
+// Read the next line from a byte slice. Return the line and the unread portion,
+// omitting the line delimiter. If there is no line delimiter present, or if
+// the array ends with a delimiter. return the
+// entire slice as the line and an empty unread portion.
+// \n and \r\n are accepted as delimiters
+func ReadLine(s []byte) (line, rest []byte) {
+	idx := bytes.IndexByte(s, '\n')
+	if idx < 0 {
+		return s, nil
+	}
+	l := idx
+	if l > 0 && s[l-1] == byte('\r') {
+		l -= 1
+	}
+	return s[:l], s[idx+1:]
+}
+
+// Split a byte array into lines
+func SplitLines(s []byte) (results [][]byte) {
+	results = make([][]byte, 0)
+	rest := s
+	for len(rest) > 0 {
+		line, r := ReadLine(rest)
+		results = append(results, line)
+		rest = r
+	}
+	return
+}
+
+// Execute a command, returning stdout, stdin, and the result as an error status
 func Execute(cmd string, args ...string) (stdout []byte, stderr []byte, err os.Error) {
 	command := exec.Command(cmd, args...)
-	command.Stdout = &byteWriter{&stdout}
-	command.Stderr = &byteWriter{&stderr}
+	wout := NewByteWriter()
+	werr := NewByteWriter()
+	command.Stdout = wout
+	command.Stderr = werr
 	err = command.Run()
-	return
+	return wout.Data(), werr.Data(), err
 }
--- mrlutils/mrlutils_test.go
+++ mrlutils/mrlutils_test.go
@@ -3,7 +3,44 @@ package mrlutils
 import (
 	"testing"
 	"strings"
+	"bytes"
 )
+
+func ok(cond bool, t *testing.T, msg string, msgParams ...interface{}) {
+	if !cond {
+		t.Errorf(msg, msgParams...)
+	}
+}
+
+func TestLines1(t *testing.T) {
+	var s []byte = []byte("foo\nbar\r\nbaz")
+	split := SplitLines(s)
+	ok(len(split) == 3, t, "Bad split length")
+	for i, val := range []string{"foo", "bar", "baz"} {
+		ok(bytes.Equal(split[i], []byte(val)), t, "Bad line %s", split[i])
+	}
+}
+
+func TestLines2(t *testing.T) {
+	var s []byte = []byte("foo\nbar\r\n\nbaz\n")
+	split := SplitLines(s)
+	ok(len(split) == 4, t, "Bad split length")
+	for i, val := range []string{"foo", "bar", "", "baz"} {
+		ok(bytes.Equal(split[i], []byte(val)), t, "Bad line %s", split[i])
+	}
+}
+
+func TestLines3(t *testing.T) {
+	var s []byte = []byte("\n")
+	split := SplitLines(s)
+	ok(len(split) == 1, t, "Bad split length")
+}
+
+func TestLines4(t *testing.T) {
+	var s []byte
+	split := SplitLines(s)
+	ok(len(split) == 0, t, "Bad split length")
+}
 
 func TestExecuteStdout(t *testing.T) {
 	stdout, stderr, err := Execute("echo", "foo")
@@ -27,6 +64,19 @@ func TestExecuteStderr(t *testing.T) {
 		t.Errorf("bad stderr <%s>", string(stderr))
 	}
 	if err == nil {
+		t.Errorf("bad error status")
+	}
+}
+
+func TestExecuteBashStderr(t *testing.T) {
+	stdout, stderr, err := Execute("bash", "-c", "echo foo 1>&2")
+	if string(stdout) != "" {
+		t.Errorf("bad stdout <%s>", string(stdout))
+	}
+	if !strings.HasSuffix(string(stderr), "foo\n") {
+		t.Errorf("bad stderr <%s>", string(stderr))
+	}
+	if err != nil {
 		t.Errorf("bad error status")
 	}
 }
