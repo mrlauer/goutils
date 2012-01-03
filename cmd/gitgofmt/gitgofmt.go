00aa2ff68722245027502446802eed2dbb943cd5
Michael Lauer <michael@mrlauer.com>
2012-01-02 22:14:12 -0500

    gitgofmt

---
+++ cmd/gitgofmt/gitgofmt.go
@@ -0,0 +1,56 @@
+//
+// gitgofmt checks that any go files in the git index are correctly formatted.
+// If not, it writes the bad files to stderr and returns an error status of 1.
+//
+package main
+
+import(
+	"fmt"
+	"mrl/utils"
+	"os"
+	"bytes"
+	"exec"
+)
+
+func main() {
+	// Get the list of files to be committed
+	stdout, _, err := utils.Execute("git", "diff", "--cached", "--name-only", "--diff-filter=ACM")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "git diff failed with %s\n", err)
+		os.Exit(1)
+	}
+	filelist := utils.SplitLines(stdout)
+	goext := []byte(".go")
+	ok := true
+	for _, file := range filelist {
+		if bytes.HasSuffix(file, goext) {
+			// Gross way to do this!
+			catcmd := exec.Command("git", "show", fmt.Sprintf(":%s", file))
+			catout, err := catcmd.StdoutPipe()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in cat %s: %s\n", file, err)
+				continue
+			}
+			fmtcmd := exec.Command("gofmt", "-l")
+			fmtcmd.Stdin = catout
+			outb := utils.NewByteWriter()
+			errb := utils.NewByteWriter()
+			fmtcmd.Stdout = outb
+			fmtcmd.Stderr = errb
+			catcmd.Start()
+			fmterr := fmtcmd.Run()
+			catcmd.Wait()
+			if fmterr != nil {
+				ok = false
+				fmt.Fprintf(os.Stderr, "syntax error in %s\n", file)
+			}
+			if len(outb.Data()) != 0 {
+				ok = false
+				fmt.Fprintf(os.Stderr, "formatting error: run gofmt -w %s\n", file)
+			}
+		}
+	}
+	if !ok {
+		os.Exit(1)
+	}
+}
--- mrl/utils/utils.go
+++ mrl/utils/utils.go
@@ -1,3 +1,5 @@
+// Package utils implements some generally useful things that I could not immediately find in
+// the standard libraries
 package utils
 
 import (
@@ -6,33 +8,35 @@ import (
 	"bytes"
 )
 
-// A writer that appends to an internal byte array
+// ByteWriter writes into a byte array.
 // (Isn't there a standard package for that?)
 type ByteWriter struct {
 	data []byte
 }
 
-// Write by appending to the internal array
+// Write appends to the internal array.
 func (w *ByteWriter) Write(b []byte) (n int, err os.Error) {
 	w.data = append(w.data, b...)
 	return len(b), nil
 }
 
-// Return the ByteWriter's data
+// Data returns the ByteWriter's byte slice.
 func (w *ByteWriter) Data() []byte {
 	return w.data
 }
 
-// Create a ByteWriter
+// NewByteWriter creates a ByteWriter initialized to an empty array.
 func NewByteWriter() *ByteWriter {
 	return &ByteWriter{}
 }
 
-// Read the next line from a byte slice. Return the line and the unread portion,
-// omitting the line delimiter. If there is no line delimiter present, or if
-// the array ends with a delimiter, return the
-// entire slice as the line and an empty unread portion.
-// \n and \r\n are accepted as delimiters
+// ReadLine reads the next line from a byte slice, 
+// return the line and the unread portion,
+// omitting the line delimiter. 
+// If there is no line delimiter present the entire slice
+// is returned as the line and nil as the unread portion.
+// \n and \r\n are accepted as delimiters.
+// Both return values are slices into the original slice, not copies.
 func ReadLine(s []byte) (line, rest []byte) {
 	idx := bytes.IndexByte(s, '\n')
 	if idx < 0 {
@@ -45,7 +49,8 @@ func ReadLine(s []byte) (line, rest []byte) {
 	return s[:l], s[idx+1:]
 }
 
-// Split a byte array into lines
+// SplitLines splits a byte slice into lines. The returned values
+// are not copies.
 func SplitLines(s []byte) (results [][]byte) {
 	rest := s
 	for len(rest) > 0 {
@@ -56,7 +61,7 @@ func SplitLines(s []byte) (results [][]byte) {
 	return
 }
 
-// Execute a command, returning stdout, stdin, and the result as an error status
+// Execute runs a command, returning stdout, stdin, and the result as an error status.
 func Execute(cmd string, args ...string) (stdout []byte, stderr []byte, err os.Error) {
 	command := exec.Command(cmd, args...)
 	wout := NewByteWriter()
